fdcdfba2ec6e57be26b42bf2408611d9f9b4cb84
walker396 <jonny396@gmail.com>
2024-09-10 20:50:27 +0930

    add monthly bill list statistic data

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,20 +1,58 @@
-import React, { useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import React, { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import "./index.scss";
 import { useDispatch } from "react-redux";
 import { getBillList } from "../../store/modules/billStore";
+import { Badge, TabBar } from "antd-mobile";
+import {
+  BillOutline,
+  AddCircleOutline,
+  CalculatorOutline,
+} from "antd-mobile-icons";
 export default function Layout() {
+  const tabs = [
+    {
+      key: "/",
+      title: "Month bills",
+      icon: <BillOutline />,
+      badge: Badge.dot,
+    },
+    {
+      key: "new",
+      title: "New bill",
+      icon: <AddCircleOutline />,
+      badge: "5",
+    },
+    {
+      key: "year",
+      title: "Year",
+      icon: <CalculatorOutline />,
+      badge: "99+",
+    },
+  ];
+
+  const [activeKey, setActiveKey] = useState("/");
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
-  }, []);
+  }, [dispatch]);
+  const navi = useNavigate();
+  const switchTab = (path) => {
+    navi(path);
+  };
   return (
-    <>
-      <div>
+    <div className="layout">
+      <div className="container">
         <Outlet />
       </div>
-      <Button color="primary">Primary</Button>
-      <div>Layout</div>
-    </>
+      <div className="footer">
+        <TabBar onChange={switchTab}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
   );
 }
--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -1,5 +1,96 @@
-import React from "react";
+import { NavBar, DatePicker } from "antd-mobile";
+import "./index.scss";
+import classNames from "classnames";
+import { useState, useMemo, useEffect } from "react";
+import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import _ from "lodash";
+const Month = () => {
+  const billList = useSelector((state) => state.bill.billList);
+  const monthList = useMemo(() => {
+    return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
+  }, [billList]);
+  const [dateVisible, setDateVisible] = useState(false);
+  const [date, setDate] = useState(dayjs().format("YYYY-MM"));
+  const [currentMonthList, setCurrentMonthList] = useState([]);
+  const monthResult = useMemo(() => {
+    console.log(currentMonthList);
+    const pay = currentMonthList
+      ?.filter((item) => item.type === "pay")
+      .reduce((pre, cur) => {
+        return pre + cur.money;
+      }, 0);
+    const income = currentMonthList
+      ?.filter((item) => item.type === "income")
+      .reduce((pre, cur) => {
+        return pre + cur.money;
+      }, 0);
+    return { pay, income, total: pay + income };
+  }, [currentMonthList]);
+  useEffect(() => {
+    const currentDate = dayjs().format("YYYY-MM");
+    if (monthList[currentDate]) {
+      setCurrentMonthList(monthList[currentDate]);
+    }
+  }, [monthList]);
 
-export default function Month() {
-  return <div>Month</div>;
-}
+  const onConfirm = (value) => {
+    const currentDate = dayjs(value).format("YYYY-MM");
+    setDate(currentDate);
+    setCurrentMonthList(monthList[currentDate]);
+    setDateVisible(false);
+  };
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        Monthly Bill
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* Time Switch Area*/}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">{date} Bill</span>
+            {/* Idea: Control the presence of the 'expand' class based on the current modal open state */}
+            <span
+              className={classNames("arrow", dateVisible && "expand")}
+            ></span>
+          </div>
+          {/* statictist area */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">{monthResult.pay}</span>
+              <span className="type">Expenses</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.income}</span>
+              <span className="type">Income</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.total}</span>
+              <span className="type">Balance</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="Bill date"
+            precision="month"
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
+            max={new Date()}
+          />
+        </div>
+        {/* 单日列表统计 */}
+        {/* {dayGroup.keys.map((key) => {
+        return (
+          <DailyBill key={key} date={key} billList={dayGroup.groupData[key]} />
+        );
+      })} */}
+      </div>
+    </div>
+  );
+};
+
+export default Month;
